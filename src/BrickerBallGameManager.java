5c655a0c07a8fd53d1d910b07bda0acd6e5f9938
Omri Tamam <omri.tamam@cs.huji.ac.il>
2021-11-16 17:42:40 +0200

    Initial commit

---
+++ src/BrickerBallGameManager.java
@@ -0,0 +1,8 @@
+import danogl.GameManager;
+import danogl.util.Vector2;
+public class BrickerBallGameManager extends GameManager {
+
+public static void main(String[] args) {
+        new GameManager("Bricker", new Vector2(700, 500)).run();
+        }
+};
